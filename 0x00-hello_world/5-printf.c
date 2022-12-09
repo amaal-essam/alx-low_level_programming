cec1c02c4090a71b2ae7aa890c0e1058bb117800
amaal-essam <amoolalmmar@gmail.com>
2022-12-08 17:11:41 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  *Return:alaways 0(Succsess)
  */
 int main(void)
-{  printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+{     	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
 }
