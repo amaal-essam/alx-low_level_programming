3a0f7ff843e8234eac52e88abb5ccc072cd15043
amaal-essam <amoolalmmar@gmail.com>
2022-12-09 17:57:44 -0800

    e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,12 +12,13 @@ int main(void)
 
 	while (ch <= 'z')
 	{
-		if (ch != 'e' || ch != 'q')
+		if (ch != 'e' && ch != 'q')
 		{
+			ch++;
 			putchar(ch);
 		}
-		ch++;
+		putchar('\n');
 	}
-	putchar('\n');
 return (0);
 }
+
