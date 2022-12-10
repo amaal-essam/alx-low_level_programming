849c3f3dfc6705008918b37c2fd7d9c9e81df45f
amaal-essam <amoolalmmar@gmail.com>
2022-12-10 06:49:13 -0800

    smile

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Return: Alaways 0(Success)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		if (n < 57)
+		{
+			putchar(44);
+
+			putchar(32);
+		}
+n++;
+	}
+putchar(n);
+return (0);
+}
