cbfb636a1b8fa74e4f53391db026db8375778485
amaal-essam <amoolalmmar@gmail.com>
2022-12-09 16:51:40 -0800

    abc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * print char in lower case
+ * Return: Alaways 0 (success)
+ */
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+return (0);
+}
