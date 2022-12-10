80d16f3986ff771aff425bde3d1470a056c75f29
amaal-essam <amoolalmmar@gmail.com>
2022-12-09 17:40:03 -0800

    e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * print char in lowercase except q,e
+ *
+ * Return: Alaways 0(Success)
+ */
+int main(void)
+{
+	char ch;
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' $$ ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+return (0);
+}
