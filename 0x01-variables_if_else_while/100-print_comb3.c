e23625aa5342bd9bbddb89152b9988ea3287b086
amaal-essam <amoolalmmar@gmail.com>
2022-12-12 04:27:35 -0800

    task9

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/*
+ * main-Entry point(Prints all possible different combination of two digits)
+ *
+ * Return: Alaways 0(Success)
+ */
+int main(void)
+{
+	int m, n;
+
+	for (n = 48; n <= 56; n++)
+
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+putchar('\n');
+return (0);
+}
