eeedcbbb796bc27c3b8f6b40c5ec3039533854cd
amaal-essam <amoolalmmar@gmail.com>
2022-12-12 04:43:02 -0800

    task9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/*
- * main-Entry point(Prints all possible different combination of two digits)
- *
+/**
+ *main -Entry point
+ *prints all possible number
  * Return: Alaways 0(Success)
  */
 int main(void)
