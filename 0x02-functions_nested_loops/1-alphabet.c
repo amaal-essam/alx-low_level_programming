e610b24393d59718dacdc92599338aca2065831c
amaal-essam <amoolalmmar@gmail.com>
2022-12-13 10:17:14 -0800

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ * print a-z to small
+ * Return: Alawys 0(Success)
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar("%c", ch);
+		ch++;
+	}
+	_putchar('\n');
+}
+int main(void)
+{
+	print_alphabet();
+return (0);
+}
