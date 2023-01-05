98aa857b89b2d019d72d0dd996ee721f2e9b479d
amaal-essam <amoolalmmar@gmail.com>
2023-01-05 08:40:22 -0800

    task3

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,4 +1,5 @@
 int _putchar(char);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-
+ioid _puts_recursion(char *s);
+ioid _print_rev_recursion(char *s);
+iint _strlen_recursion(char *s);
+int factorial(int n);
